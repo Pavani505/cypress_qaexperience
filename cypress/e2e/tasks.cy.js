7585a03a61647577f29e628d123fcb3dd0ebd14b
Pavani <andreluizspavani@gmail.com>
2024-05-10 19:37:14 -0300

    encapsulando codigo

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -6,6 +6,14 @@ const urlApiBase = 'http://localhost:3333'
 
 describe('tasks', () => {
 
+  // Ex de criar massa de dados com const
+  // const taskName = {
+  //   name: 'blablabla'
+  //   id_done: false
+  // }
+
+  // para chamar basta usar taskName.name ou taskName.id_done
+
   // function listarTarefas() {
   //   var todasAsTarefas = []
   //   cy.request({
@@ -77,11 +85,7 @@ describe('tasks', () => {
 
   it('deve cadastrar uma nova tarefa', () => {
     removerTarefaNome('Ler um livro')
-    cy.visit('/')
-    cy.title()
-      .should('eq', 'Gerencie suas tarefas com Mark L')
-    cy.get('#newTask')
-      .type('Ler um livro{enter}')
+    cy.createTask('Ler um livro')
     cy.contains('main div p', 'Ler um livro')
       .should('be.visible')
     // cy.wait (9999)
@@ -90,14 +94,23 @@ describe('tasks', () => {
   it('não deve cadastrar uma nova tarefa ja existente', () => {
     removerTarefaNome('Ler um livro')
     criarTarefa('Ler um livro')
-    cy.visit('/')
-    cy.title()
-      .should('eq', 'Gerencie suas tarefas com Mark L')
-    cy.get('#newTask')
-      .type('Ler um livro{enter}')
+    cy.createTask('Ler um livro')
+    // cy.visit('/')
+    // cy.title()
+    //   .should('eq', 'Gerencie suas tarefas com Mark L')
+    // cy.get('#newTask')
+    //   .type('Ler um livro{enter}')
     cy.get('#swal2-html-container')
       .should('be.visible')
       .should('have.text', 'Task already exists!')
   })
 
+})
+
+Cypress.Commands.add('createTask', (taskName) => {
+  cy.visit('/')
+  cy.title()
+    .should('eq', 'Gerencie suas tarefas com Mark L')
+  cy.get('#newTask')
+    .type(taskName + '{enter}')
 })
